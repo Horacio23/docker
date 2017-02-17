52d04a1b878707b1ce5a4aca4580a0bba3bd1c2a
Horacio <horacio.delgado23@gmail.com>
2017-02-17 11:48:41 -0500

    added new commands and formatted readme

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,6 @@
 #Docker Aliases
+alias drmdang='docker volume rm $(docker volume ls -f dangling=true -q)'
+alias drmi='docker rmi $1'
 alias drma="docker rm $(docker ps -a -q)"
 alias dims="docker images"
 alias dps="docker ps"
